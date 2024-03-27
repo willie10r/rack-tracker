66923138a5290fd7ff50ba7cc50910445722b904
willie m <zx10rwillie@gmail.com>
2024-03-26 20:27:59 -0500

    Adding base styling to the mobile size on the tracker page.

--- src/app/(pages)/tracker/page.js
+++ src/app/(pages)/tracker/page.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from '../../../styles/base.module.scss'
+import stylestracker from '../../../styles/tracker.module.scss'
 
 
 
@@ -8,164 +9,184 @@ export default function Tracker() {
     return(
         <main >
             <section>
-                <h2>
-                    Status
+                <h2 className={stylestracker.statusIndecatorsH}>
+                    Status Indecators
                 </h2>
-                <ul className={styles.removeDot}>
-                    <li>
-                        <span className={styles.dot}></span>
+                <ul className={[styles.removeDot, styles.paddingRemove, stylestracker.statusIndecators].join(' ')}>
+                    <li className={stylestracker.indiText}>
+                        <span className={[styles.dot, styles.green, stylestracker.indecators].join(' ')}></span>
                         No wait
                     </li>
-                    <li>
-                        <span className={styles.dot}></span>
+                    <li className={stylestracker.indiText}>
+                        <span className={[styles.dot, styles.blue, stylestracker.indecators].join(' ')}></span>
                         Small wait
                     </li>
-                    <li>
-                        <span className={styles.dot}></span>
+                    <li className={stylestracker.indiText}>
+                        <span className={[styles.dot, styles.yellow, stylestracker.indecators].join(' ')}></span>
                         30-60 min wait
                     </li>
-                    <li>
-                        <span className={styles.dot}></span>
+                    <li className={stylestracker.indiText}>
+                        <span className={[styles.dot, styles.red, stylestracker.indecators].join(' ')}></span>
                         3-4hr wait
                     </li>
-                    <li>
-                        <span className={styles.dot}></span>
+                    <li className={stylestracker.indiText}>
+                        <span className={[styles.dot, styles.black, stylestracker.indecators].join(' ')}></span>
                         Rack down
                     </li>
-                    <li>
-                        <span className={styles.dot}></span>
+                    <li className={stylestracker.indiText}>
+                        <span className={[styles.dot, styles.orange, stylestracker.indecators].join(' ')}></span>
                         Out of allocation
                     </li>
                 </ul>
             </section>
             <section>
-                <h2>
+                <h2 className={stylestracker.title}>
                     Status
                 </h2>
                 <div>
-                    <h3>
+                    <h3 className={stylestracker.rackName}>
                         Kinder Morgan
                     </h3>
-                    <ul>
-                        <li>
-                            <div>
-                            <span className={styles.dot}></span>
-                                <p>12:50pm</p>
+                    <ul className={[styles.removeDot, styles.paddingRemove, stylestracker.cardGroup ].join(' ')}>
+                        <li className={stylestracker.eachCard}>
+                            <div className={stylestracker.candt}>
+                            <span className={[styles.dot, styles.green, stylestracker.indiLight].join(' ')}></span>
+                                <p className={stylestracker.time}>12:50pm</p>
                                 </div>
-                            <div>
-                                <p>WM</p>
-                                <input></input>
+                            <div className={stylestracker.iandn}>
+                                <p className={stylestracker.ini}>WM</p>
+                               <p className={stylestracker.note}>This is the notes area</p>
                             </div>
                         </li>
-                        <li>
-                            <div>
-                            <span className={styles.dot}></span>
-                                <p>12:50pm</p>
+                        <li className={stylestracker.eachCard}>
+                            <div className={stylestracker.candt}>
+                            <span className={[styles.dot, styles.blue, stylestracker.indiLight].join(' ')}></span>
+                                <p className={stylestracker.time}>12:50pm</p>
                                 </div>
-                            <div>
-                                <p>WM</p>
-                                <input></input>
+                            <div className={stylestracker.iandn}>
+                                <p className={stylestracker.ini}>WM</p>
+                               <p className={stylestracker.note}>This is the notes area</p>
                             </div>
                         </li>
-                        <li>
-                            <div>
-                            <span className={styles.dot}></span>
-                                <p>12:50pm</p>
+                        <li className={stylestracker.eachCard}>
+                            <div className={stylestracker.candt}>
+                            <span className={[styles.dot, styles.red, stylestracker.indiLight].join(' ')}></span>
+                                <p className={stylestracker.time}>12:50pm</p>
                                 </div>
-                            <div>
-                                <p>WM</p>
-                                <input></input>
+                            <div className={stylestracker.iandn}>
+                                <p className={stylestracker.ini}>WM</p>
+                               <p className={stylestracker.note}>This is the notes area</p>
                             </div>
                         </li>
                     </ul>
                 </div>
                 <div>
-                    <h3>
+                    <h3 className={stylestracker.rackName}>
                         Magellan
                     </h3>
-                    <ul>
-                    <li>
-                            <div>
-                            <span className={styles.dot}></span>
-                                <p>12:50pm</p>
+                    <ul className={[styles.removeDot, styles.paddingRemove, stylestracker.cardGroup ].join(' ')}>
+                    <li className={stylestracker.eachCard}>
+                            <div className={stylestracker.candt}>
+                            <span className={[styles.dot, styles.blue, stylestracker.indiLight].join(' ')}></span>
+                                <p className={stylestracker.time}>12:50pm</p>
                                 </div>
-                            <div>
-                                <p>WM</p>
-                                <input></input>
+                            <div className={stylestracker.iandn}>
+                                <p className={stylestracker.ini}>WM</p>
+                               <p className={stylestracker.note}>This is the notes area</p>
                             </div>
                         </li>
-                        <li>
-                            <div>
-                            <span className={styles.dot}></span>
-                                <p>12:50pm</p>
+                        <li className={stylestracker.eachCard}>
+                            <div className={stylestracker.candt}>
+                            <span className={[styles.dot, styles.blue, stylestracker.indiLight].join(' ')}></span>
+                                <p className={stylestracker.time}>12:50pm</p>
                                 </div>
-                            <div>
-                                <p>WM</p>
-                                <input></input>
+                            <div className={stylestracker.iandn}>
+                                <p className={stylestracker.ini}>WM</p>
+                               <p className={stylestracker.note}>This is the notes area</p>
                             </div>
                         </li>
-                        <li>
-                            <div>
-                            <span className={styles.dot}></span>
-                                <p>12:50pm</p>
+                        <li className={stylestracker.eachCard}>
+                            <div className={stylestracker.candt}>
+                            <span className={[styles.dot, styles.blue, stylestracker.indiLight].join(' ')}></span>
+                                <p className={stylestracker.time}>12:50pm</p>
                                 </div>
-                            <div>
-                                <p>WM</p>
-                                <input></input>
+                            <div className={stylestracker.iandn}>
+                                <p className={stylestracker.ini}>WM</p>
+                               <p className={stylestracker.note}>This is the notes area</p>
                             </div>
                         </li>
                     </ul>
                 </div>
                 <div>
-                    <h3>
+                    <h3 className={stylestracker.rackName}>
                         U.S. Oil
                     </h3>
-                    <ul>
-                    <li>
-                            <div>
-                            <span className={styles.dot}></span>
-                                <p>12:50pm</p>
+                    <ul className={[styles.removeDot, styles.paddingRemove, stylestracker.cardGroup ].join(' ')}>
+                    <li className={stylestracker.eachCard}>
+                            <div className={stylestracker.candt}>
+                            <span className={[styles.dot, styles.blue, stylestracker.indiLight].join(' ')}></span>
+                                <p className={stylestracker.time}>12:50pm</p>
                                 </div>
-                            <div>
-                                <p>WM</p>
-                                <input></input>
+                            <div className={stylestracker.iandn}>
+                                <p className={stylestracker.ini}>WM</p>
+                               <p className={stylestracker.note}>This is the notes area</p>
                             </div>
                         </li>
-                        <li>
-                            <div>
-                            <span className={styles.dot}></span>
-                                <p>12:50pm</p>
+                        <li className={stylestracker.eachCard}>
+                            <div className={stylestracker.candt}>
+                            <span className={[styles.dot, styles.blue, stylestracker.indiLight].join(' ')}></span>
+                                <p className={stylestracker.time}>12:50pm</p>
                                 </div>
-                            <div>
-                                <p>WM</p>
-                                <input></input>
+                            <div className={stylestracker.iandn}>
+                                <p className={stylestracker.ini}>WM</p>
+                               <p className={stylestracker.note}>This is the notes area</p>
+                            </div>
+                        </li>
+                        <li className={stylestracker.eachCard}>
+                            <div className={stylestracker.candt}>
+                            <span className={[styles.dot, styles.blue, stylestracker.indiLight].join(' ')}></span>
+                                <p className={stylestracker.time}>12:50pm</p>
+                                </div>
+                            <div className={stylestracker.iandn}>
+                                <p className={stylestracker.ini}>WM</p>
+                               <p className={stylestracker.note}>This is the notes area</p>
                             </div>
                         </li>
                     </ul>
                 </div>
                 <div>
-                    <h3>
+                    <h3 className={stylestracker.rackName}>
                         Enterprise
                     </h3>
-                    <ul>
-                    <li>
-                            <div>
-                            <span className={styles.dot}></span>
-                                <p>12:50pm</p>
+                    <ul className={[styles.removeDot, styles.paddingRemove, stylestracker.cardGroup ].join(' ')}>
+                    <li className={stylestracker.eachCard}>
+                            <div className={stylestracker.candt}>
+                            <span className={[styles.dot, styles.blue, stylestracker.indiLight].join(' ')}></span>
+                                <p className={stylestracker.time}>12:50pm</p>
                                 </div>
-                            <div>
-                                <p>WM</p>
-                                <input></input>
+                            <div className={stylestracker.iandn}>
+                                <p className={stylestracker.ini}>WM</p>
+                               <p className={stylestracker.note}>This is the notes area</p>
                             </div>
                         </li>
-                        <li>
-                            <div>
-                            <span className={styles.dot}></span>
-                                <p>12:50pm</p>
+                        <li className={stylestracker.eachCard}>
+                            <div className={stylestracker.candt}>
+                            <span className={[styles.dot, styles.blue, stylestracker.indiLight].join(' ')}></span>
+                                <p className={stylestracker.time}>12:50pm</p>
                                 </div>
-                            <div>
-                                <p>WM</p>
-                                <input></input>
+                            <div className={stylestracker.iandn}>
+                                <p className={stylestracker.ini}>WM</p>
+                               <p className={stylestracker.note}>This is the notes area</p>
+                            </div>
+                        </li>
+                        <li className={stylestracker.eachCard}>
+                            <div className={stylestracker.candt}>
+                            <span className={[styles.dot, styles.blue, stylestracker.indiLight].join(' ')}></span>
+                                <p className={stylestracker.time}>12:50pm</p>
+                                </div>
+                            <div className={stylestracker.iandn}>
+                                <p className={stylestracker.ini}>WM</p>
+                               <p className={stylestracker.note}>This is the notes area</p>
                             </div>
                         </li>
                     </ul>
